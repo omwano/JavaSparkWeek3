a541deac65680bd441761939dec88f627fd43864
Omwano <abekahellena@gmail.com>
2023-11-11 04:13:37 +0300

    add Animals class

--- src/main/java/com/niawetu/EndangeredAnimals.java
+++ src/main/java/com/niawetu/EndangeredAnimals.java
@@ -1,4 +1,17 @@
 package com.niawetu;
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
 
+import java.util.List;
+import java.util.Objects;
 public class EndangeredAnimals {
+    private String health;
+    private String age;
+    private String name;
+      this.name=name;
+
+
+        this.health=health;
+
+        this.age = age;
 }
